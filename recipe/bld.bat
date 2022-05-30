3bdff4c65105a03969450921e1c11de49e8f4d36
Filipe Brandão <fdabrandao@gmail.com>
2022-05-30 15:37:08 +0100

    Install ampltools using pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 del /s *.so
 del /s *.dylib
+%PYTHON% -m pip install ampltools
 %PYTHON% -m pip install . --no-deps --ignore-installed -vv
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python -m pip install ampltools
 python -m pip install . --no-deps --ignore-installed -vv
