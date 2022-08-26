2fce6d6f23a1cc5aa8f1524d3eeb93c8c5a3bbfe
Filipe Brandão <fdabrandao@gmail.com>
2022-08-26 16:21:40 +0100

    Do not remove ampltools import

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i~ 's/, "ampltools"//g' setup.py
-sed -i~ 's/import ampltools as tools/pass/g' amplpy/__init__.py
+# sed -i~ 's/import ampltools as tools/pass/g' amplpy/__init__.py
 python -m pip install . --no-deps --ignore-installed -vv
